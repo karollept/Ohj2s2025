142c26c6d37c12e4ac08cbf56e4ba6fa59663a3a
karollept <karoliina.lepisto@metropolia.fi>
2025-10-22 11:30:08 +0300

    mod9 teht 4 started

--- mod-9/classes/auto.py
+++ mod-9/classes/auto.py
@@ -1,5 +1,7 @@
+import random
+
 class Auto:
-    def __init__(self, rekisteritunnus, huippunopeus, nopeus=0, kuljettu_matka=0):
+    def __init__(self, rekisteritunnus, nopeus=0, kuljettu_matka=0, huippunopeus = random.randint(100, 200)):
         self.rekisteritunnus = rekisteritunnus
         self.huippunopeus = huippunopeus
         self.nopeus = nopeus
--- mod-9/teht4.py
+++ mod-9/teht4.py
@@ -11,3 +11,29 @@
 
 #Kilpailu jatkuu, kunnes jokin autoista on edennyt vähintään 10000 kilometriä.
 # Lopuksi tulostetaan kunkin auton kaikki ominaisuudet selkeäksi taulukoksi muotoiltuna.
+
+from classes.auto import Auto
+import random
+
+
+auto1 = Auto("ABC-1")
+auto2 = Auto("ABC-2")
+auto3 = Auto("ABC-3")
+auto4 = Auto("ABC-4")
+auto5 = Auto("ABC-5")
+auto6 = Auto("ABC-6")
+auto7 = Auto("ABC-7")
+auto8 = Auto("ABC-8")
+auto9 = Auto("ABC-9")
+auto10 = Auto("ABC-10")
+
+while Auto max(kuljettu_matka)<1000:
+    Auto.kiihdyta()
+
+print(f"""
+Lopuksi auton kuljettua 1,5 tuntia:
+Auton rekisteritunnus on {auto1.rekisteritunnus}.
+Auton huippunopeus on  {auto1.huippunopeus} km/h.
+Tämänhetkinen auton nopeus on {auto1.nopeus} km/h.
+Autolla kuljettu matka on {auto1.kuljettu_matka} km.
+""")
