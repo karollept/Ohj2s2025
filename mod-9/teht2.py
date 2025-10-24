0520273f2b526dc90cf0730fca188683ae1449c5
karollept <karoliina.lepisto@metropolia.fi>
2025-10-24 19:43:55 +0300

    text to own

--- mod-9/teht2.py
+++ mod-9/teht2.py
@@ -1,3 +1,12 @@
+#Jatka ohjelmaa kirjoittamalla Auto-luokkaan kiihdytä-metodi, joka saa parametrinaan nopeuden muutoksen (km/h).
+# Jos nopeuden muutos on negatiivinen, auto hidastaa. Metodin on muutettava auto-olion nopeus-ominaisuuden arvoa.
+# Auton nopeus ei saa kasvaa huippunopeutta suuremmaksi eikä alentua nollaa pienemmäksi.
+# Jatka pääohjelmaa siten, että auton nopeutta nostetaan ensin +30 km/h, sitten +70 km/h ja lopuksi +50 km/h.
+# Tulosta tämän jälkeen auton nopeus.
+# Tee sitten hätäjarrutus määräämällä nopeuden muutos -200 km/h ja tulosta uusi nopeus.
+# Kuljettua matkaa ei tarvitse vielä päivittää.
+
+
 from classes.auto import Auto
 
 auto1 = Auto("ABC-123", 142)
