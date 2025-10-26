e479908f0917fc3cabb932fcf17e969b7080f409
karollept <karoliina.lepisto@metropolia.fi>
2025-10-26 18:24:51 +0200

    mod11 teht2 valmis

--- classes/auto.py
+++ classes/auto.py
@@ -25,5 +25,20 @@ class Auto:
         self.kuljettu_matka = self.kuljettu_matka + self.aika * self.nopeus
 
 
+class Sahkoauto(Auto):
+    def __init__(self, rekisteritunnus, huippunopeus, nopeus, akkukapasiteetti, kuljettu_matka=0):
+        self.akkukapasiteetti = akkukapasiteetti
+        super().__init__(rekisteritunnus, huippunopeus, nopeus, kuljettu_matka)
 
+    def tulosta_tiedot(self):
+        print(f"Auton {self.rekisteritunnus} matkamittarilukemat ovat:"
+              f"nopeus {self.nopeus}, kuljettu matka {self.kuljettu_matka}, akkukapasiteetti {self.akkukapasiteetti} ja auton huippunopeus on {self.huippunopeus}.")
 
+class Polttomoottoriauto(Auto):
+    def __init__(self, rekisteritunnus, huippunopeus, nopeus, bensatankin_koko, kuljettu_matka=0):
+        self.bensatankin_koko = bensatankin_koko
+        super().__init__(rekisteritunnus, huippunopeus, nopeus, kuljettu_matka)
+
+    def tulosta_tiedot(self):
+        print(f"Auton {self.rekisteritunnus} matkamittarilukemat ovat:"
+              f"nopeus {self.nopeus}, kuljettu matka {self.kuljettu_matka}, bensatankin koko {self.bensatankin_koko} ja auton huippunopeus on {self.huippunopeus}.")
--- mod-11/teht2.py
+++ mod-11/teht2.py
@@ -8,3 +8,16 @@
 # yhden polttomoottoriauton (ACD-123, 165 km/h, 32.3 l).
 # Aseta kummallekin autolle haluamasi nopeus,
 # käske autoja ajamaan kolmen tunnin verran ja tulosta autojen matkamittarilukemat.
+
+from classes.auto import Auto
+from classes.auto import Sahkoauto
+from classes.auto import Polttomoottoriauto
+
+autot = []
+autot.append(Sahkoauto("ABC-15", 180,45, 52.5))
+autot.append(Polttomoottoriauto("ACD-123", 165,40,32.3))
+
+for a in autot:
+    a.kulje(3)
+    a.tulosta_tiedot()
+
